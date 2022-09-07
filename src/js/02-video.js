7bf2c6349ee1e4174ea592d31003d1a2a8d801f2
vadimvatsenko <69381493+vadimvatsenko@users.noreply.github.com>
2022-09-07 19:28:02 +0300

    update 07.09.2022 19:27

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,48 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+// вынос локального хранилища в переменную
+const CURRENT_TIME_KEY = "videoplayer-current-time"
+
+const iframePlayer = document.querySelector('#vimeo-player');
+
+const player = new Player(iframePlayer);
+
+player.on('play', function() {
+    console.log('played the video!');
+
+});
+
+player.getVideoTitle().then(function(title) {
+    console.log('title:', title);
+});
+
+const onPlay = data => {
+    localStorage.setItem(CURRENT_TIME_KEY, data.seconds);
+    console.log(data.seconds);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));//Слушатель раз в 1сек
+
+const getCurrentTime = Number(localStorage.getItem(CURRENT_TIME_KEY));
+// console.log(typeof getCurrentTime);
+
+player.setCurrentTime(getCurrentTime).then(function(seconds) {
+   console.log(seconds)
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
+
+
+
+
+
 
