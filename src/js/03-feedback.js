954cf4a45c43b4e53363c6851404af16ac29eca5
vadimvatsenko <69381493+vadimvatsenko@users.noreply.github.com>
2022-09-10 18:17:12 +0300

    10.09.2022 18:17

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,28 +2,25 @@ import throttle from 'lodash.throttle';
 
 const FEEDBACK_FORM_STATE = "feedback-form-state";
 
+let saveLocalData = {};//сохранить массив с данными
+
 const refs = {
   form: document.querySelector('.feedback-form'),
   input: document.querySelector('.feedback-form input'),
   textarea: document.querySelector('.feedback-form textarea'),
 };
 
-getLocalData(refs.input, refs.textarea);
+getLocalData();//вызов получения данных с хранилища
 
 refs.form.addEventListener("submit", handleSubmit);
 refs.form.addEventListener("input", throttle(saveLocalInput, 500));
-
-// refs.textarea.addEventListener('input', throttle(onTextareaInput, 500));
-// refs.input.addEventListener('input', throttle(onTextareaInput, 500));
-
-getLocalData(refs.input, refs.textarea);
 
 function handleSubmit(event) {
   event.preventDefault();
   const {
     elements: { email, message }
   } = event.currentTarget;
-
+  
   if (email.value === "" || message.value === "") {
     return alert("Please fill in all the fields!");
   }
@@ -35,25 +32,35 @@ function handleSubmit(event) {
   event.currentTarget.reset();
 
   localStorage.removeItem(FEEDBACK_FORM_STATE);
+
+  console.log(userData)
 };
+
 
 function saveLocalInput(event) {
+
+  saveLocalData[event.target.name] = event.target.value;
+
+  localStorage.setItem(FEEDBACK_FORM_STATE, JSON.stringify(saveLocalData));
   
-  const saveLocalData = JSON.stringify({ [event.target.name]: event.target.value });
-  
-  localStorage.setItem(FEEDBACK_FORM_STATE, saveLocalData);
 };
 
-function getLocalData(email, textarea) {
-  const getSavedData = localStorage.getItem(FEEDBACK_FORM_STATE);
+function getLocalData() {
+  const getSavedData = localStorage.getItem(FEEDBACK_FORM_STATE) || "";
+
   const writeData = JSON.parse(getSavedData);
 
-  console.log(writeData);
-
-  email.value = writeData.email 
-  textarea.value = writeData.textarea;
-
+  if (writeData) {
+    refs.input.value = writeData.email;
+    refs.textarea.value = writeData.message;
+  }
 };
+
+
+
+
+
+
 
 
 
